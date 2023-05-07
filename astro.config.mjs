ae1ed57a15354bd6f46dd8a2d1eacab766d69428
tilmanmixyz <tilmanmixyz@proton.me>
2023-05-07 16:04:43 +0200

    change out and public dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  outDir: 'public',
+  publicDir: 'static',
+});
